fee3e187d720526a86789d3340ef2eaba420299a
seamanhd <seamanhd@gmail.com>
2017-05-06 13:17:30 -0400

    got webpack working

---
+++ app/app.js
@@ -0,0 +1,7 @@
+// REACT STUFF --------------------------------
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var Header = require("./components/Header");
+
+ReactDOM.render(<Header />, document.getElementById("logo"));
